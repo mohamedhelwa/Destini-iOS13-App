6dcb798994d36353ee2ccf05237f6178fb8488f4
Mohamed <moh_helwa@hotmail.com>
2020-05-03 16:36:59 +0200

    last

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by MoHaMeD on 4/9/20.
+//  Copyright © 2020 MoHaMeD. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
